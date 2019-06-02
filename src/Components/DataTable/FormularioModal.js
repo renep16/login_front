4c46c74a121f78ee4cbc3c7b5c9d39ca30032729
Rene Peña <renealejandrop16@gmail.com>
2019-06-01 22:35:19 -0400

    Finalizar interfaz sin API de Usuarios

--- src/Components/DataTable/FormularioModal.js
+++ src/Components/DataTable/FormularioModal.js
@@ -1,18 +1,267 @@
 import React, { Component } from 'react';
-import { Modal, ModalBody, ModalFooter, ModalHeader, Button } from "reactstrap";
+import {
+  Modal,
+  ModalBody,
+  ModalFooter,
+  ModalHeader,
+  Button,
+  FormGroup,
+  Label,
+  Input,
+  Col,
+  Form,
+  Row,
+  FormFeedback,
+  FormText
+} from "reactstrap";
+
+//Origninal State
+const originalState = {
+  usuario: '',
+  clave: '',
+  persona: '',
+  rol: 1,
+  activo: true,
+  validUsuario: true,
+  validClave: true,
+  usuarioExiste: false,
+  estadoForm: 1 //estado puede ser 1 agregar, 2 consultar, 3 modificar
+}
+
+const validacionUsuario = /^[0-9a-zA-Z]+$/;
+const validacionClave = /^[0-9a-zA-Z]+$/;
 
 class FormularioModal extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = originalState;
+  }
+
+  componentDidMount() {
+    this.setState(originalState);
+    const idItem = this.props.idItem;
+    if (idItem !== 0) {
+      this.setState({ estadoForm: 2 });
+      fetch('./usuarios.json')
+        .then(data => data.json())
+        .then(data => {
+          const item = data[0];
+          this.setState({
+            usuario: item['usuario'],
+            clave: '',
+            persona: item['nombre'],
+            rol: item['idrol'],
+            activo: (item['activo'] === 1) ? true : false,
+            validUsuario: true,
+            validClave: true
+          });
+        })
+        .catch(error => console.log(error));
+    }
+
+  }
+
+  handleChange = (e) => {
+    const name = e.target.name;
+    switch (name) {
+      case 'activo':
+        this.setState(prevState => ({
+          activo: !prevState.activo
+        }));
+        break;
+      case 'usuario': {
+        //validaUsuario
+        if (e.target.value.length > 4 && e.target.value.length < 16) {
+          this.setState({ validUsuario: validacionUsuario.test(e.target.value) });
+        } else {
+          this.setState({ validUsuario: false });
+        }
+
+        this.setState({ [e.target.name]: e.target.value });
+        break;
+      }
+      case 'clave': {
+        //valida clave
+        if (e.target.value.length > 4 && e.target.value.length < 16) {
+          this.setState({ validClave: validacionClave.test(e.target.value) });
+        } else {
+          this.setState({ validClave: false });
+        }
+        this.setState({ [e.target.name]: e.target.value });
+        break;
+      }
+
+      default:
+        this.setState({ [e.target.name]: e.target.value });
+        break;
+    }
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    if (this.state.validClave && this.state.validUsuario) {
+      this.setState(originalState);
+      this.props.aceptarForm();
+    }
+
+  }
+
+  modificarForm = () => {
+    this.setState({ estadoForm: 3 });
+  }
+
+  eliminarForm = () => {
+    switch (this.state.estadoForm) {
+      case 1:
+        this.props.cancelarForm();
+        break;
+      case 2:
+        this.props.eliminarForm();
+        break;
+      default:
+        this.setState({ estadoForm: 2 });
+        break;
+    }
+  }
+
   render() {
+    let titulo = this.props.itemForm + ' - ';
+    switch (this.state.estadoForm) {
+      case 1:
+        titulo += 'Agregar';
+        break;
+      case 2:
+        titulo += 'Consultar';
+        break;
+      case 3:
+        titulo += 'Modificar';
+        break;
+      default:
+        titulo += 'Agregar';
+        break;
+    }
     return (
-      <Modal isOpen={this.props.muestraForm} toggle={this.props.toggleForm}>
-        <ModalHeader toggle={this.props.toggleForm}>Modal title</ModalHeader>
-        <ModalBody>
-          {this.props.idItem}
-      </ModalBody>
-        <ModalFooter>
-          <Button color="primary" onClick={this.props.aceptarForm}>Do Something</Button>{' '}
-          <Button color="secondary" onClick={this.props.cancelarForm}>Cancel</Button>
-        </ModalFooter>
+      <Modal isOpen={this.props.muestraForm} toggle={this.props.toggleForm} size="lg" centered backdrop="static">
+        <Form onSubmit={this.handleSubmit}>
+          <ModalHeader toggle={this.props.toggleForm}>{titulo}</ModalHeader>
+          <ModalBody>
+            <Row form>
+              <Col md={5}>
+                <FormGroup>
+                  <Label for="usuarioForm">Usuario</Label>
+                  <Input
+                    type="text"
+                    name="usuario"
+                    id="usuarioForm"
+                    placeholder="Ingrese nombre de usuario"
+                    value={this.state.usuario}
+                    onChange={this.handleChange}
+                    required
+                    minLength="5"
+                    maxLength="15"
+                    valid={(this.state.validUsuario && (this.state.usuario !== ''))}
+                    invalid={!this.state.validUsuario}
+                    disabled={(this.state.estadoForm !== 1)}
+                  />
+                  <FormFeedback className={(this.state.usuarioExiste) ? 'hide' : ''}>Formato inválido</FormFeedback>
+                  <FormFeedback className={(!this.state.usuarioExiste) ? 'hide' : ''} >El usuario ya existe</FormFeedback>
+                  <FormText>Debe tener entre 5 y 15 caracteres (no especiales) y/o números.</FormText>
+                </FormGroup>
+              </Col>
+              <Col md={5}>
+                <FormGroup>
+                  <Label for="claveForm">Clave</Label>
+                  <Input
+                    type="password"
+                    name="clave"
+                    id="claveForm"
+                    placeholder="Ingrese clave"
+                    value={this.state.clave}
+                    onChange={this.handleChange}
+                    required={this.state.estadoForm === 1}
+                    minLength="5"
+                    maxLength="15"
+                    valid={this.state.validClave && (this.state.clave !== '')}
+                    invalid={!this.state.validClave}
+                    disabled={(this.state.estadoForm === 2)}
+                  />
+                  <FormFeedback >Formato inválido</FormFeedback>
+                  <FormText>Debe tener entre 5 y 15 caracteres (no especiales) y/o números.</FormText>
+                </FormGroup>
+              </Col>
+              <Col md={2}>
+                <FormGroup check>
+                  <Label ></Label>
+                  <Input
+                    type="checkbox"
+                    name="activo"
+                    id="activoForm"
+                    value="1"
+                    checked={this.state.activo}
+                    onChange={this.handleChange}
+                    required
+                    disabled={(this.state.estadoForm === 2)}
+                  />
+                  <Label for="activoForm" check>Activo</Label>
+                </FormGroup>
+              </Col>
+            </Row>
+            <Row form>
+              <Col md={6}>
+                <FormGroup>
+                  <Label for="personaForm">Persona</Label>
+                  <Input
+                    type="text"
+                    name="persona"
+                    id="personaForm"
+                    placeholder="Nombre completo"
+                    value={this.state.persona}
+                    onChange={this.handleChange}
+                    required
+                    disabled={(this.state.estadoForm === 2)}
+                  />
+                </FormGroup>
+              </Col>
+              <Col md={6}>
+                <FormGroup>
+                  <Label for="rolForm">Rol</Label>
+                  <Input
+                    type="select"
+                    name="rol"
+                    id="rolForm"
+                    value={this.state.rol}
+                    onChange={this.handleChange}
+                    required
+                    disabled={(this.state.estadoForm === 2)}
+                  >
+                    <option value="1">Administrador</option>
+                    <option value="2">Vendedor</option>
+                  </Input>
+                </FormGroup>
+              </Col>
+            </Row>
+
+          </ModalBody>
+          <ModalFooter>
+            <Button
+              color="secondary"
+              onClick={this.modificarForm}
+              disabled={(this.state.estadoForm !== 2)}>
+              Modificar
+          </Button>{' '}
+            <Button
+              color="danger"
+              onClick={this.eliminarForm}>
+              {(this.state.estadoForm !== 2) ? 'Cancelar' : 'Eliminar'}
+            </Button>{' '}
+            <Button
+              color="success"
+              disabled={(this.state.estadoForm === 2)}>
+              Aceptar
+          </Button>
+          </ModalFooter>
+        </Form>
       </Modal>
     );
   }
--- src/Components/Usuarios/Usuarios.js
+++ src/Components/Usuarios/Usuarios.js
@@ -5,6 +5,9 @@ import Busqueda from '../DataTable/Busqueda';
 import Tabla from '../DataTable/Tabla';
 import FormularioModal from '../DataTable/FormularioModal';
 
+import ReactPlaceholder from 'react-placeholder';
+import "react-placeholder/lib/reactPlaceholder.css";
+
 /**
  * Componente que renderiza la pantalla de Usuario
  * Estado:
@@ -25,15 +28,18 @@ export default class Usuarios extends Component {
         '#', 'Usuario', 'Nombre', 'Activo', 'Rol'
       ],
       muestraForm: false,
-      idUsuarioForm: 0
+      mountForm: false,
+      idUsuarioForm: 0,
+      tablaCargando: true
     };
   }
 
   getUsuarios = () => { //Obtiene todos los usuarios
+    this.setState({ tablaCargando: false });
     fetch('./usuarios.json')
       .then(data => data.json())
       .then(data => {
-        this.setState({ listaUsuarios: data });
+        this.setState({ listaUsuarios: data, tablaCargando: true });
       })
       .catch(error => console.log(error));;
   }
@@ -41,6 +47,7 @@ export default class Usuarios extends Component {
   getTodos = () => { //si pulsas el boton todos
     this.setState({ criterioBusqueda: '*' });
     this.getUsuarios();
+
   }
 
   limpiarTabla = () => { //si pulsas el boton limpiar
@@ -70,16 +77,24 @@ export default class Usuarios extends Component {
   }
 
   handleItemClick = (e) => {
-    this.setState({ 
+    this.setState({
       idUsuarioForm: e,
-      muestraForm: true
-     });
+      muestraForm: true,
+      mountForm: true
+    });
   }
 
   toggleForm = () => {
     this.setState(prevState => ({
       muestraForm: !prevState.muestraForm
     }));
+    if(this.state.mountForm === true){
+      setTimeout(()=>{
+        this.setState({ mountForm: false });
+      }, 500)
+    }else{
+      this.setState({ mountForm: true });
+    }
   }
 
   render() {
@@ -97,22 +112,28 @@ export default class Usuarios extends Component {
               limpiarTabla={this.limpiarTabla}
             />
           </div>
-          <Tabla
-            data={this.state.listaUsuarios}
-            columnas={this.state.columnas}
-            onItemClick={this.handleItemClick}
-          />
+          <ReactPlaceholder type='rect' showLoadingAnimation ready={this.state.tablaCargando} style={{ width: "100%", height: 420 }} color='#E0E0E0'>
+            <Tabla
+              data={this.state.listaUsuarios}
+              columnas={this.state.columnas}
+              onItemClick={this.handleItemClick}
+            />
+          </ReactPlaceholder>
+
         </div>
-        <FormularioModal
-          muestraForm={this.state.muestraForm}
-          toggleForm={this.toggleForm}
-          idItem={this.state.idUsuarioForm}
-          aceptarForm={this.toggleForm}
-          cancelarForm={this.toggleForm}
-        />
 
-
-
+        {this.state.mountForm && (
+          <FormularioModal
+            muestraForm={this.state.muestraForm}
+            toggleForm={this.toggleForm}
+            idItem={this.state.idUsuarioForm}
+            aceptarForm={this.toggleForm}
+            eliminarForm={this.toggleForm}
+            modificarForm={this.toggleForm}
+            cancelarForm={this.toggleForm}
+            itemForm="Usuario"
+          />
+        )}
       </div>
     )
   }
